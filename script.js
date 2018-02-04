566963400e1cac4a5f59e0e73d3989ce580bd251
Michael Gregoire <mdgregoire@gmail.com>
2018-02-04 12:57:44 -0600

    added the monthly calc

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ $(document).ready(function(){
     else if(employeeInput.eFirstName && employeeInput.eLastName && employeeInput.idNumber && employeeInput.jobTitle && employeeInput.annualSalary){
       employeeArray.push(employeeInput);
       $('#salaryCount').html(function(i, val) {
-         return parseInt(val)+parseInt(employeeInput.annualSalary);});//end salary count, converts salary to a number
+        var totalAnnualSalary = parseInt(val)+parseInt(employeeInput.annualSalary);
+        calculateMonthlySalary (totalAnnualSalary);
+        return parseInt(val)+parseInt(employeeInput.annualSalary);});//end salary count, calls calculateMonthlySalary function and converts salary to a number
       $('.inputField').each(function(){$(this).val("");});//resets the input fields after a sucessful creation
       appendInput (employeeArray);
     }//end data check -- true and if so write to the DOM
@@ -31,7 +33,8 @@ $(document).ready(function(){
   $('#clearAll').on('click', function (){
     $('#tbody').empty();
     $('#salaryCount').html(function(i, val) {
-       return 0;});//end salary clear
+       return 0;});
+       calculateMonthlySalary(0);//end salary clear
        employeeArray = [];//resetting employee array
   });//end clear all button
 });//end document ready
@@ -50,13 +53,19 @@ function appendInput (arrayToWrite){
   }//end for loop that writes employee data
 }//end appendInput function
 
+function calculateMonthlySalary(salary){
+$('#monthyCount').html(function(i, val){
+    return (salary/12).toFixed(0);});
+}//end calculateMonthlySalary function
+
 function removeEmployee (idToRemove){
   for (i=0; i<employeeArray.length; i++){
     if(idToRemove == employeeArray[i].idNumber){  //uses id number incase of duplicate last names
       $('#salaryCount').html(function(index, val){
+        calculateMonthlySalary(val-employeeArray[i].annualSalary);//calls calculateMonthlySalary with the adjusted salary numbers
         return val-employeeArray[i].annualSalary;});
       employeeArray.splice(i, 1);
       appendInput (employeeArray);//calls the appendInput function, passing it the spliced array
     }//end if statement
   }//end for loop to check for and remove employee number
-}//end removeEmployee
+}//end removeEmployee function
