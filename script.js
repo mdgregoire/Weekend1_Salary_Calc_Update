83dd7b9a7d29282988b1c31de2dc22d24df422a0
Michael Gregoire <mdgregoire@gmail.com>
2018-02-03 21:26:12 -0600

    append is working

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-console.log('js');
-
-var employeeArray = [];
-
+var employeeArray = []; //global Array for working with Employee Data
 
 $(document).ready(function(){
-  console.log('JQ');
-
 //get employee data from DOM fields and Button click
-
 $('#submitButton').on('click', function(){
-  console.log('inbuttonclick');
   var employeeInput = {
     eFirstName : $('#eFirstName').val(),
     eLastName : $('#eLastName').val(),
@@ -17,17 +10,23 @@ $('#submitButton').on('click', function(){
     jobTitle : $('#jobTitle').val(),
     annualSalary : $('#annualSalary').val(),
   };//end create employeeInput
+
+if(employeeInput.eFirstName && employeeInput.eLastName && employeeInput.idNumber && employeeInput.jobTitle && employeeInput.annualSalary){
   employeeArray.push(employeeInput);//pushes to the employeeArray
-  console.log(employeeArray);
-
-
+  $('#salaryCount').html(function(i, val) {
+     return val*1+parseInt(employeeInput.annualSalary);});//end salary count
   $('.inputField').each(function(){$(this).val("");});//end clear fields function
 
-
-
-
-
+  appendInput (employeeInput);
+}//end data check -- true
+else{
+  alert ('Error! Please enter all required fields.');
+}//end data check -- false
 });//end on click submit
 
+function appendInput (newData){
+console.log(newData);
+$('.employeeTable').append('<tr><td>'+newData.eLastName+'</td><td>'+newData.eFirstName+'</td><td>'+newData.idNumber+'</td><td>'+newData.jobTitle+'</td><td>'+newData.annualSalary+'</td><td><button class="removeData" data-eLastName="' + newData.eLastName + '">Remove</button></td></tr>');
+}//end appendInput function
 
-});
+});//end document ready
