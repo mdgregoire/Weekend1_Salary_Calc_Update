79e57a0dccff9d004e24161d5137b2c31024b373
Michael Gregoire <mdgregoire@gmail.com>
2018-02-03 20:20:30 -0600

    created input fields that self clear

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 console.log('js');
-$(document).ready(readyNow);
 
-function readyNow(){
+var employeeArray = [];
+
+
+$(document).ready(function(){
   console.log('JQ');
-}
+
+//get employee data from DOM fields and Button click
+
+$('#submitButton').on('click', function(){
+  console.log('inbuttonclick');
+  var employeeInput = {
+    eFirstName : $('#eFirstName').val(),
+    eLastName : $('#eLastName').val(),
+    idNumber : $('#idNumber').val(),
+    jobTitle : $('#jobTitle').val(),
+    annualSalary : $('#annualSalary').val(),
+  };//end create employeeInput
+  employeeArray.push(employeeInput);//pushes to the employeeArray
+  console.log(employeeArray);
+
+
+  $('.inputField').each(function(){$(this).val("");});//end clear fields function
+
+
+
+
+
+});//end on click submit
+
+
+});
